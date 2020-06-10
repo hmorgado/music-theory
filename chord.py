2fca3b0bab497f29c3976dc7b277102c83c1ccac
Heitor Morgado <heitorm.morgado@gmail.com>
2020-06-10 09:15:55 -0600

    master simpler code

--- chord.py
+++ chord.py
@@ -2,10 +2,3 @@ import random
 import notes
 
 notes = notes.Notes()
-
-print(notes.get_scale('D', 'min'))
-print(notes.get_scale('C', 'maj'))
-print(notes.get_scale('F', 'min_pent'))
-print(notes.get_scale('A', 'min_pent'))
-
-exit()
--- notes.py
+++ notes.py
@@ -2,10 +2,14 @@ class Notes:
 
 	notes = ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#']
 	
-	major_scale = [0, 2, 4, 5, 7, 9, 11, 12]
-	minor_scale = [0, 2, 3, 5, 7, 8, 10, 12]
-	minor_pent  = [0, 3, 5, 7, 10]
-	major_pent  = [0, 2, 4, 7, 9]
+	patterns = {
+		'maj'      : [0, 2, 4, 5, 7, 9, 11, 12],
+		'min'      : [0, 2, 3, 5, 7, 8, 10, 12],
+		'maj_pent' : [0, 2, 4, 7, 9],
+		'min_pent' : [0, 3, 5, 7, 10],
+	}
+
+	# https://www.pythonanywhere.com/
 
 	# needs self cuz its a class method, not a function
 	def custom_notes(self, n):
@@ -13,15 +17,10 @@ class Notes:
 		return self.notes[n_idx:len(self.notes)] + self.notes[0:n_idx]
 
 	def get_scale(self, n, kind='maj'):
-		if kind == 'maj':
-			scale_pattern = self.major_scale
-		elif kind == 'min':
-			scale_pattern = self.minor_scale
-		elif kind == 'min_pent':
-			scale_pattern = self.minor_pent
-		elif kind == 'maj_pent':
-			scale_pattern = self.major_pent
-		
+		if kind not in self.patterns:
+			raise Exception(f'scale [{kind}] not found.')
+
+		scale_pattern = self.patterns[kind]
 		return list(self.custom_notes(n)[i % 12] for i in scale_pattern)
 
 	def __init__(self):
